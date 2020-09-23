882fe1d6c62ec0d7bf8cb1a46813b33e508c847a
tnakagawa <nakagat@gmail.com>
2020-09-23 10:17:46 +0900

    update

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -3,6 +3,97 @@
 async function init() {
     await testGOTP();
     await testTOTP();
+    testBase32();
+    document.getElementById("issuer").addEventListener("change", createURI);
+    document.getElementById("account").addEventListener("change", createURI);
+    document.getElementById("secret").addEventListener("change", createURI);
+    document.getElementById("algorithm").addEventListener("change", createURI);
+    document.getElementById("digits").addEventListener("change", createURI);
+    document.getElementById("genkey").addEventListener("click", setSecret);
+    setSecret();
+    setCode();
+}
+
+function createURI() {
+    let issuer = encodeURI(document.getElementById("issuer").value);
+    let account = encodeURI(document.getElementById("account").value);
+    let secret = encodeURI(document.getElementById("secret").value);
+    let algorithm = document.getElementById("algorithm").value;
+    let digits = document.getElementById("digits").value;
+    let uri = "otpauth://totp/" + issuer + ":" + account
+        + "?secret=" + secret + "&issuer=" + issuer;
+    if (algorithm) {
+        uri += "&algorithm=" + algorithm;
+    }
+    if (digits) {
+        uri += "&digits=" + digits;
+    }
+    // uri += "&period=30";
+    document.getElementById("uri").innerText = uri;
+
+    let qr = new QRCode(0, 0, "8bit");
+    qr.addData(uri);
+    qr.make();
+    let size = 5;
+    let c = document.getElementById("qr");
+    c.width = (qr.modules.length + 8) * size;
+    c.height = (qr.modules[0].length + 8) * size;
+    let ctx = c.getContext('2d');
+    ctx.fillStyle = 'black';
+    for (let row = 0; row < qr.modules.length; row++) {
+        for (let col = 0; col < qr.modules[row].length; col++) {
+            if (qr.modules[row][col]) {
+                ctx.fillRect((row + 4) * size, (col + 4) * size, size, size);
+            }
+        }
+    }
+    document.getElementById("qr_wrapper").style.width = c.width + "px";
+    before = 0;
+}
+
+function setSecret() {
+    let algorithm = document.getElementById("algorithm").value;
+    let size = 20;
+    if (algorithm == "SHA256") {
+        size = 32;
+    } else if (algorithm == "SHA512") {
+        size = 64;
+    }
+    let bs = new Uint8Array(size);
+    window.crypto.getRandomValues(bs);
+    let base32 = Base32.Enc(bs);
+    document.getElementById("secret").value = base32;
+    createURI();
+}
+
+var before = 0;
+
+function setCode() {
+    let time = (new Date()).getTime();
+    let now = Math.floor(time / 1000);
+    if (before <= now) {
+        let otp = new OTP();
+        let algorithm = document.getElementById("algorithm").value;
+        let digits = document.getElementById("digits").value;
+        if (algorithm == "SHA256") {
+            otp.Algorithm = "SHA-256";
+        } else if (algorithm == "SHA512") {
+            otp.Algorithm = "SHA-512";
+        }
+        if (digits) {
+            otp.Digit = parseInt(digits, 10);
+        }
+        let secret = document.getElementById("secret").value
+        otp.totp(Base32.Dec(secret), now).then(otp => {
+            let half = otp.length / 2;
+            document.getElementById("otp").innerText = otp.slice(0, half) + " " + otp.slice(half);
+        });
+        before = Math.floor(now / 30) * 30 + 30;
+    }
+    let val = ((before * 1000 - time) / 1000).toFixed(1);
+    document.getElementById("time").innerHTML = val + "s";
+    document.getElementById("timebar").value = val;
+    setTimeout(setCode, 100);
 }
 
 async function testGOTP() {
@@ -62,6 +153,28 @@ async function testTOTP() {
                 console.log(otp, otps[i * 3 + j], otp == otps[i * 3 + j]);
             });
         }
+    }
+}
+
+function testBase32() {
+    // BASE32("") = ""
+    // BASE32("f") = "MY======"
+    // BASE32("fo") = "MZXQ===="
+    // BASE32("foo") = "MZXW6==="
+    // BASE32("foob") = "MZXW6YQ="
+    // BASE32("fooba") = "MZXW6YTB"
+    // BASE32("foobar") = "MZXW6YTBOI======"
+    var keys = ["", "f", "fo", "foo", "foob", "fooba", "foobar",];
+    var vals = ["", "MY======", "MZXQ====", "MZXW6===", "MZXW6YQ=", "MZXW6YTB", "MZXW6YTBOI======",];
+    let enc = new TextEncoder();
+    for (let i = 0; i < keys.length; i++) {
+        let base32 = Base32.Enc(enc.encode(keys[i]));
+        console.log(base32, vals[i], base32 == vals[i]);
+    }
+    for (let i = 0; i < vals.length; i++) {
+        let bs = Base32.Dec(vals[i]);
+        let key = new TextDecoder().decode(bs);
+        console.log(key, keys[i], key == keys[i]);
     }
 }
 
--- crypto/otp.js
+++ crypto/otp.js
@@ -1,3 +1,4 @@
+"use strict";
 
 class OTP {
     constructor() {
@@ -42,7 +43,7 @@ class OTP {
                         ((hash[offset + 1] & 0xff) << 16) |
                         ((hash[offset + 2] & 0xff) << 8) |
                         (hash[offset + 3] & 0xff);
-                    let otp = binary % Math.pow(10, this.Digit);
+                    let otp = binary % (10 ** this.Digit);
                     let result = "" + otp;
                     while (result.length < this.Digit) {
                         result = "0" + result;
@@ -60,3 +61,78 @@ class OTP {
     }
 }
 
+// https://tools.ietf.org/html/rfc4648
+class Base32 {
+    static Enc(data) {
+        let bs = new Uint8Array(data);
+        let base32 = "";
+        for (let i = 0; i < bs.length; i += 5) {
+            let src = bs.slice(i, i + 5);
+            let pos = [0, 0, 0, 0, 0, 0, 0, 0];
+            switch (src.length) {
+                default:
+                    pos[7] = src[4] & 0x1f;
+                    pos[6] = (src[4] >> 5) & 0x07;
+                case 4:
+                    pos[6] |= (src[3] << 3) & 0x18;
+                    pos[5] = (src[3] >> 2) & 0x1f;
+                    pos[4] = (src[3] >> 7) & 0x01;
+                case 3:
+                    pos[4] |= (src[2] << 1) & 0x1e;
+                    pos[3] = (src[2] >> 4) & 0x0f;
+                case 2:
+                    pos[3] |= (src[1] << 4) & 0x10;
+                    pos[2] = (src[1] >> 1) & 0x1f;
+                    pos[1] = (src[1] >> 6) & 0x03;
+                case 1:
+                    pos[1] |= (src[0] << 2) & 0x1c;
+                    pos[0] = (src[0] >> 3) & 0x1f;
+            }
+            let size = Math.ceil(src.length * 8 / 5);
+            for (let j = 0; j < size; j++) {
+                base32 += "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".charAt(pos[j]);
+            }
+            for (let j = 0; j < 8 - size; j++) {
+                base32 += "=";
+            }
+        }
+        return base32;
+    }
+    static Dec(base32) {
+        let ret = base32.match(/^([A-Z2-7]{8})*([A-Z2-7]{7}=|[A-Z2-7]{5}===|[A-Z2-7]{4}====|[A-Z2-7]{2}======)?$/);
+        if (ret == null) {
+            return null;
+        }
+        let pad = base32.match(/=*$/)[0];
+        let size = Math.floor((base32.length - pad.length) * 5 / 8);
+        let bs = new Uint8Array(size);
+        let tmp = [];
+        for (let i = 0; i < base32.length - pad.length; i++) {
+            tmp.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".indexOf(base32.charAt(i)));
+        }
+        let round = base32.length / 8;
+        for (let i = 0; i < round; i++) {
+            let src = tmp.slice(i * 8, i * 8 + 8);
+            switch (src.length) {
+                case 8:
+                    bs[i * 5 + 4] = tmp[i * 8 + 7]
+                        | ((tmp[i * 8 + 6] << 5) & 0xe0);
+                case 7:
+                    bs[i * 5 + 3] = ((tmp[i * 8 + 6] >> 3) & 0x03)
+                        | ((tmp[i * 8 + 5] << 2) & 0x7c)
+                        | ((tmp[i * 8 + 4] << 7) & 0x80);
+                case 5:
+                    bs[i * 5 + 2] = ((tmp[i * 8 + 4] >> 1) & 0x0f)
+                        | ((tmp[i * 8 + 3] << 4) & 0xf0);
+                case 4:
+                    bs[i * 5 + 1] = ((tmp[i * 8 + 3] >> 4) & 0x01)
+                        | ((tmp[i * 8 + 2] << 1) & 0x3e)
+                        | ((tmp[i * 8 + 1] << 6) & 0xc0);
+                case 2:
+                    bs[i * 5] = ((tmp[i * 8 + 1] >> 2) & 0x07)
+                        | ((tmp[i * 8] << 3) & 0xf8);
+            }
+        }
+        return bs;
+    }
+}
